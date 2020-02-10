35d3b778d95dbf395cb2ec9b2522313d1700288e
Max Willian <maxwillian63@gmail.com>
2020-02-10 17:49:50 -0300

    Mudando driver para gmail

--- index.js
+++ index.js
@@ -59,12 +59,7 @@ exports.generateXls = (req, res) => {
 sendEmail = (fileBuffer) => {
     console.log('iniciando envio via email');
     let transporter = nodemailer.createTransport({
-        host: "smtp-mail.outlook.com",
-        secureConnection: false, // TLS requires secureConnection to be false
-        port: 587, // port for secure SMTP
-        tls: {
-            ciphers:'SSLv3'
-        },
+        service: "gmail",
         auth: {
             user: process.env.NODEMAILER_EMAIL,
             pass: process.env.NODEMAILER_PASS
